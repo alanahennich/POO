dba83bbd841374e15b4dbba721b6db2ab383f3f5
Alana Hennich <106201906+alanahennich@users.noreply.github.com>
2026-09-06 15:07:54 -0300

    Add files via upload

---
+++ 01-introdução/Ola_mundo.java
@@ -0,0 +1,7 @@
+/* 1. Criar um programa simples em Java que exiba a mensagem “Olá, Mundo!”.*/
+
+public class Ola_mundo{
+    public static void main(String[] args){
+        System.out.println("Olá, mundo!");
+    }
+}
